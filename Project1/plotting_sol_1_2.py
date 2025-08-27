81d636baa182557a49fb816124414f9f8d3a367d
victorbj <victorbj@uio.no>
2025-08-27 10:41:27 +0200

    Changed the plotting script

--- Project1/plotting_sol_1_2.py
+++ Project1/plotting_sol_1_2.py
@@ -1,10 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-data = np.loadtxt('diff_eq_sol.txt')
+# Path to this script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "diff_eq_sol.txt")
+
+data = np.loadtxt(file_path)
 
 x = data[:, 0]
 u = data[:, 1]
 
 plt.plot(x, u)
+plt.xlabel("x")
+plt.ylabel("u(x)")
+plt.title("Solution to the Differential Equation")
+plt.grid(alpha=0.3)
+plt.tight_layout()
+plt.savefig("Plot2")
 plt.show()
+plt.close()
