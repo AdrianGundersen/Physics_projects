70625e744f249d00eecac4cdf784ce4c6ec682d7
casperaj <casperaj@uio.no>
2025-08-27 14:11:35 +0200

    laster opp filen med oppgave 7 til git

---
+++ Project1/problem_7.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <numbers> 
+#include <cmath>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+//using namespace std;
+
+std::ofstream ofile;
+
+// Incert argumentes and vectors
+int main(int argc, char* argv[])
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    char *outfilename;
+    int i, j, n;
+    double h;
+    //Gir en feilmelding om du ikke har med rette argumenter
+    if( argc <= 1)
+    {
+        std::cout << "Bad Usage: " << argv[0] << 
+        " write the dssired filname of output file" << std::endl;
+        exit(1);
+    }
+    else{
+        outfilename = argv[1];
+    }
+    ofile.open(outfilename);
+    std::cout << "Read in number of mech points" << std::endl;
+
+    //Leser av anntall iterasjoner du skriver i terminalen
+    std::cin >> n;
+
+    //steglende
+    h = 1.0/( (double) n+1);
+
+    // vector med x- verdier fra [0,1]
+    std::vector<double> x_values;
+    for (int j = 0; j < n; j++){
+        x_values.push_back(j*h);
+    };
+    x_values.push_back(1.0);
+
+    std::vector<double> a(n, -1.0);  // diagonalvektor a
+    std::vector<double> b(n, 2.0);  // diagonalvektor b
+    std::vector<double> c(n, -1.0);  // diagonalvektor c
+
+    std::vector<double> v(n);       // vektoren v
+    std::vector<double> g(n, 0.0);  // løsningen g
+    std::vector<double> temp(n, 0.0);  // midletidig
+
+    for (int i = 0; i < n; i++){
+        double x = (i+1)*h;     // vi må hoppe over randbetingeleser
+        v[i] = 1 - (1-std::exp(-10.0))*x_values[i]-std::exp(-10.0*x_values[i]);
+    }
+
+    // Forward sub
+    double btemp =  b[0];
+
+    for (int i = 1; i < n; i++) {
+        temp[i] = c[i-1] / btemp;
+        btemp = b[i] - a[i] * temp[i];
+        g[i] = (v[i] - a[i]*g[i-1]) / btemp;
+    }
+
+    //backward sub
+    for (int i= n-2; i >=0; i--) {
+        g[i] -= temp[i+1] * g[i+1]; 
+    }
+
+    int width = 12;
+    int prec = 4;
+
+    for (int i = 0; i <= n; i++){
+    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x_values[i]
+          << std::setw(width) << std::setprecision(prec) << std::scientific << g[i]
+          << std::endl;
+        }
+    ofile.close();
+
+    // Calculate duration
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Time taken: " << duration.count() << " seconds\n";
+
+    return 0;
+}
