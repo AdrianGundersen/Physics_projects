a3bc37616b0da9ceedc50d399d60b41a7c208555
victorbj <victorbj@uio.no>
2025-08-27 11:53:35 +0200

    changed formatting of plot

--- Project1/plotting_sol_1_2.py
+++ Project1/plotting_sol_1_2.py
@@ -1,8 +1,20 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Path to this script's directory
+# Oppdaterer mpl parametere
+plt.rcParams.update({
+    'font.size': 14,
+    'figure.figsize': (6, 4),
+    'axes.titlesize': 16,
+    'axes.labelsize': 14,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'lines.linewidth': 2,
+    'legend.fontsize': 10,
+    'figure.dpi': 300,
+})
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "diff_eq_sol.txt")
 
