4d0d8f231ec0f37f562972d035ec0165a3c8f92b
victorbj <victorbj@uio.no>
2025-08-30 21:19:12 +0200

    documented code

--- Project1/problem_1_2.cpp
+++ Project1/problem_1_2.cpp
@@ -12,35 +12,40 @@ std::vector<double> u_x(std::vector<double> x_vals);
 int main(){
     std::string filename = "diff_eq_sol.txt";
 
-    auto start = std::chrono::high_resolution_clock::now();
-    std::vector<double> x_values;
-    int n = 100000;
-    double dx = 1.0/n;
+    // Starts timer
+    auto start = std::chrono::high_resolution_clock::now(); 
+    std::vector<double> x_values; // initialize x-array
+    int n = 100000; // # of points
+    double dx = 1.0/n; 
+
+    // creates x-array
     for (int j = 0; j < n; j++){
         x_values.push_back(j*dx);
     };
-    x_values.push_back(1.0);
+    x_values.push_back(1.0); // adds boundary
 
-    std::vector<double> results = u_x(x_values);
+    std::vector<double> results = u_x(x_values); // calculates y-values
     
-    int width = 12;
-    int prec = 4;
+    int width = 12; // aligns columns
+    int prec = 4; // # of digits (precision)
 
+    // starts writing to file
     std::ofstream ofile;
     ofile.open(filename);
 
     for (int i = 0; i <= n; i++){
     ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x_values[i]
           << std::setw(width) << std::setprecision(prec) << std::scientific << results[i]
-          << std::endl;
+          << "\n";
         }
     ofile.close();
-    // End timer
+    // ends writing to file
+
+    // Ends timer
     auto end = std::chrono::high_resolution_clock::now();
 
     // Calculate duration
     std::chrono::duration<double> duration = end - start;
-
     std::cout << "Time taken: " << duration.count() << " seconds\n";
     
 }
@@ -51,10 +56,10 @@ std::vector<double> u_x(std::vector<double> x_vals){
     const double e = 2.71828182845904523536;
 
     for(int i = 0; i < size + 1; i++){
-        double u = 1 - (1-std::exp(-10.0))*x_vals[i]-std::exp(-10.0*x_vals[i]);
+        double u = 1 - (1-std::exp(-10.0))*x_vals[i]-std::exp(-10.0*x_vals[i]); // exact solution
         u_values.push_back(u);
     }
-    u_values.push_back(0.0);
+    u_values.push_back(0.0); // boundary value
 
     return u_values;
 };
