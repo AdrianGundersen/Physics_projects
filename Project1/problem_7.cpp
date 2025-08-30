4e2ed5087b39fabc449edb8be24f15edf02d896f
victorbj <victorbj@uio.no>
2025-08-30 21:44:31 +0200

    Updated comments and made some minor corrections

--- Project1/problem_7.cpp
+++ Project1/problem_7.cpp
@@ -11,18 +11,18 @@
 
 std::ofstream ofile;
 
-// Incert argumentes and vectors
+// Insert argumentes and vectors
 int main(int argc, char* argv[])
 {
     auto start = std::chrono::high_resolution_clock::now();
     char *outfilename;
     int i, j, n;
     double h;
-    //Gir en feilmelding om du ikke har med rette argumenter
+    // Gir en feilmelding om du ikke har med rette argumenter
     if( argc <= 1)
     {
         std::cout << "Bad Usage: " << argv[0] << 
-        " write the dssired filname of output file" << std::endl;
+        " write the desired filename of output file" << std::endl;
         exit(1);
     }
     else{
@@ -31,10 +31,10 @@ int main(int argc, char* argv[])
     ofile.open(outfilename);
     std::cout << "Read in number of mech points" << std::endl;
 
-    //Leser av anntall iterasjoner du skriver i terminalen
+    //Reads # of mesh points
     std::cin >> n;
 
-    //steglende
+    //step size
     h = 1.0/( (double) n+1);
 
     // vector med x- verdier fra [0,1]
@@ -44,44 +44,49 @@ int main(int argc, char* argv[])
     };
     x_values.push_back(1.0);
 
-    std::vector<double> a(n, -1.0);  // diagonalvektor a
-    std::vector<double> b(n, 2.0);  // diagonalvektor b
-    std::vector<double> c(n, -1.0);  // diagonalvektor c
+    std::vector<double> a(n, -1.0);  // superdiagonal a
+    std::vector<double> b(n, 2.0);   // diagonal b
+    std::vector<double> c(n, -1.0);  // subdiagonal c
 
-    std::vector<double> v(n);       // vektoren v
-    std::vector<double> g(n, 0.0);  // løsningen g
-    std::vector<double> temp(n, 0.0);  // midletidig
+    std::vector<double> v(n);        // approximate solution v
+    std::vector<double> g(n, 0.0);   // right-hand side g
+    std::vector<double> temp(n, 0.0);  // temporary vector
 
+    // builds g-vector (RHS)
     for (int i = 0; i < n; i++){
-        double x = (i+1)*h;     // vi må hoppe over randbetingeleser
-        v[i] = 1 - (1-std::exp(-10.0))*x_values[i]-std::exp(-10.0*x_values[i]);
+        double x = (i+1)*h;     // x-values from 0 to 1 (w/o boundaries)
+        g[i] = h*h * 100.0 * std::exp(-10.0 * x);
     }
 
     // Forward sub
-    double btemp =  b[0];
+    double btemp = b[0];
+    v[0] = g[0] / btemp; // initialize solution at first interior
 
     for (int i = 1; i < n; i++) {
         temp[i] = c[i-1] / btemp;
-        btemp = b[i] - a[i] * temp[i];
-        g[i] = (v[i] - a[i]*g[i-1]) / btemp;
+        btemp   = b[i] - a[i] * temp[i];
+        v[i]    = (g[i] - a[i]*v[i-1]) / btemp;   // compute solution into v
     }
 
-    //backward sub
-    for (int i= n-2; i >=0; i--) {
-        g[i] -= temp[i+1] * g[i+1]; 
+    // backward sub
+    for (int i = n-2; i >= 0; i--) {
+        v[i] -= temp[i+1] * v[i+1]; // back-substitute into v
     }
 
+    // sets precision and width of output
     int width = 12;
     int prec = 4;
 
+    v.push_back(0.0);  // adds boundary value
+
     for (int i = 0; i <= n; i++){
-    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x_values[i]
-          << std::setw(width) << std::setprecision(prec) << std::scientific << g[i]
-          << std::endl;
-        }
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << x_values[i]
+              << std::setw(width) << std::setprecision(prec) << std::scientific << v[i]
+              << std::endl;
+    }
     ofile.close();
 
-    // Calculate duration
+    // Writes duration of the program
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     std::cout << "Time taken: " << duration.count() << " seconds\n";
