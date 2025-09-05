ebd94b667ff436ba92d72bff125d5ba5e5a63c24
casperaj <casperaj@uio.no>
2025-09-05 10:48:44 +0200

    la til så  vi plotter for n=10000 også

--- Project1/plot_problem_8.py
+++ Project1/plot_problem_8.py
@@ -24,15 +24,17 @@ file_path = os.path.join(script_dir)
 folder = "output"
 data_file_1 = "problem_8_1000.txt"
 data_file_2 = "problem_8_100000.txt"
+data_file_3 = "problem_8_10000.txt"
 
 data_1 = np.loadtxt(os.path.join(folder, data_file_1))
 data_2 = np.loadtxt(os.path.join(folder, data_file_2))
+data_3 = np.loadtxt(os.path.join(folder, data_file_3))
 
-x_1, x_2 = data_1[:, 0], data_2[:, 0]
-v_1, v_2 = data_1[:, 1], data_2[:, 1]
-u_1, u_2 = data_1[:, 2], data_2[:, 2]
-delta_1, delta_2 = data_1[:, 3], data_2[:, 3]
-epsilon_1, epsilon_2 = data_1[:, 4], data_2[:, 4]
+x_1, x_2, x_3 = data_1[:, 0], data_2[:, 0], data_3[:, 0]
+v_1, v_2, v_3 = data_1[:, 1], data_2[:, 1], data_3[:, 1]
+u_1, u_2, u_3 = data_1[:, 2], data_2[:, 2], data_3[:, 1]
+delta_1, delta_2, delta_3 = data_1[:, 3], data_2[:, 3], data_3[:, 3]
+epsilon_1, epsilon_2, epsilon_3 = data_1[:, 4], data_2[:, 4], data_3[:, 4]
 
 """
 x_1, x_2 = x_1[1:-1], x_2[1:-1]
@@ -46,6 +48,7 @@ title_absolute = "Logaritmical_absolute_error"
 title_relative = "Logaritmical_relative_error"
 
 plt.plot(x_1, delta_1, label="n=1000")
+plt.plot(x_3, delta_3, label="n=10000")
 plt.plot(x_2, delta_2, label="n=100000")
 plt.xlabel(r"$x$")
 plt.ylabel(r"$\log_{10}\!\left|u - v\right|$")
@@ -58,6 +61,7 @@ plt.show()
 plt.close()
 
 plt.plot(x_1, epsilon_1, label=r"$n=1000$")
+plt.plot(x_3, epsilon_3, label=r"$n=10000$")
 plt.plot(x_2, epsilon_2, label=r"$n=100000$")
 plt.xlabel(r"$x$")
 plt.ylabel(r"$\log_{10}\!\left|\frac{u-v}{u}\right|$")
