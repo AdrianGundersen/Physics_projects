f351a0c88c74614abe6e0cefb2e0e8754a050be0
victorbj <victorbj@uio.no>
2025-09-10 14:47:16 +0200

    Fixed bugs causing major timing errors in problem10

--- Project1/problem_10.cpp
+++ Project1/problem_10.cpp
@@ -34,15 +34,13 @@ void problem_10(){
         int n = std::pow(10.0, j+1);  
         double h = 1.0/(n+1);
         
-        std::vector<double> a(n, -1.0);  // superdiagonal a
+        std::vector<double> a(n-1, -1.0);  // subdiagonal a
         std::vector<double> b(n, 2.0);   // diagonal b
-        std::vector<double> c(n, -1.0);  // subdiagonal c
+        std::vector<double> c(n-1, -1.0);  // superdiagonal c
 
-        std::vector<double> v(n);        // approximate solution v
+        std::vector<double> v(n, 0.0);        // approximate solution v
         std::vector<double> g(n, 0.0);   // right-hand side g
 
-        std::vector<double> gtemp = g;  // temporary vector
-        std::vector<double> btemp = b;   // diagonal b temp vector
 
         // builds g-vector (RHS)
         for (int i = 0; i < n; i++){
@@ -50,11 +48,14 @@ void problem_10(){
             g[i] = h * h * 100.0 * std::exp(-10.0 * x);
         }
 
+        // start timing
         auto start = std::chrono::high_resolution_clock::now();
-        // running opt algo
-        for(int p = 0; p < 1000; p++){
 
-           
+        // running optimalized algo 1000 times
+        for(int p = 0; p < 1000; p++){
+            std::vector<double> gtemp = g;  // temporary vector
+            std::vector<double> btemp = b;   // diagonal b temp vector
+            std::vector<double> v_10 = v;   // subdiagonal c temp vector
 
             // Forward sub
 
@@ -64,37 +65,33 @@ void problem_10(){
             }
 
             // backward sub
-            v[n-1] = gtemp[n-1] / btemp[n-1];
+            v_10[n-1] = gtemp[n-1] / btemp[n-1];
 
             for (int i = n-2; i >= 0; i--) {
-                v[i] = (gtemp[i] + v[i+1]) / btemp[i]; // back-substitute into v
+                v_10[i] = (gtemp[i] + v_10[i+1]) / btemp[i]; // back-substitute into v
             }
-            v.insert(v.begin(), 0.0);
-            v.push_back(0.0);  
+            // do not add boundary values here as we only look at time for algo
     }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
         time_opt.push_back(duration.count());
         
-        // Problem 8 vectors 
-        std::vector<double> v_8(n);        // approximate solution v
-        std::vector<double> g_8(n, 0.0);   // right-hand side g
-
-        std::vector<double> gtemp_8 = g;  // temporary vector
-        std::vector<double> btemp_8 = b;   // diagonal b temp vector
+        // Problem 8 vectors (standard algo)
+        // start timing
 
         auto start2 = std::chrono::high_resolution_clock::now();
         for(int p = 0; p < 1000; p++){
             // builds g-vector (RHS)
-
-            
+            std::vector<double> v_8 = v;        // approximate solution v
+            std::vector<double> gtemp_8 = g;  // temporary vector
+            std::vector<double> btemp_8 = b;   // diagonal b temp vector
             
             // Forward sub
 
             for (int i = 1; i < n; i++) {
                 double k_i = a[i] / btemp_8[i-1];
-                btemp_8[i]   = b[i] - c[i-1] * k_i;
-                gtemp_8[i] = g[i] - k_i * gtemp_8[i-1];
+                btemp_8[i] = btemp_8[i] - c[i-1] * k_i;
+                gtemp_8[i] = gtemp_8[i] - k_i * gtemp_8[i-1];
             }
 
             // backward sub
@@ -103,9 +100,7 @@ void problem_10(){
             for (int i = n-2; i >= 0; i--) {
                 v_8[i] = (gtemp_8[i] -c[i]* v_8[i+1]) / btemp_8[i]; // back-substitute into v
             }
-            // adds boundary values
-            v_8.insert(v_8.begin(), 0.0);
-            v_8.push_back(0.0);
+            // do not add boundary values here as we only look at time for algo
     }
         auto end2 = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration2 = end2 - start2;
