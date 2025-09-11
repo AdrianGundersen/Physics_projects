d7174bb24f82e4b5961c6e1bcb3076a0de9fb519
victorbj <victorbj@uio.no>
2025-09-11 10:06:36 +0200

    Initialize matrix and compute evals for problem 2

--- Project2/problem2.cpp
+++ Project2/problem2.cpp
@@ -4,11 +4,7 @@
 #include <cmath>
 
 
-int main(){
-    int N = 6;
-    double a = -1.;
-    double b = 1.;
-    double c = -2.;
+arma::Mat<double> create_tridiagonal(int N, double a, double b, double c){
     arma::Mat<double> A(N, N, arma::fill::zeros);
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
@@ -23,5 +19,17 @@ int main(){
             }
         }
     }
-    A.print();
+    return A;
+}
+
+int main(){
+    int N = 6;
+    double h = 1./(N+1);
+    double a = -1/(h*h);
+    double b = 2/(h*h);
+    double c = -1/(h*h);   
+    arma::Mat<double> A = create_tridiagonal(N, a, b, c);
+    arma::vec eigvals = arma::eig_sym(A);
+    A.print("Matrix A: ");
+    eigvals.print("Eigenvalues: ");
 }
