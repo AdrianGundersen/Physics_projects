cfec5c5c5edbcb06b708b7e9418e8e3a6365a503
victorbj <victorbj@uio.no>
2025-09-15 20:27:23 +0200

    changed to iterate over cols before rows in tridiag.cpp

--- Project2/tridiag.cpp
+++ Project2/tridiag.cpp
@@ -30,8 +30,8 @@ double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){
 
     double max_val = std::abs(A(k,l)); // initial max value
 
-    for (int i = 0; i < N; i++){
-        for (int j = i+ 1 ; j < N; j++){ // only look at upper triangle
+    for (int j = 1; j < N; j++){ // columns first for cache efficiency
+        for (int i = 0; i < j; i++){  // i < j ensures upper triangle
             double abs_val = std::abs(A(i, j));
             if (abs_val > max_val){
                 max_val = abs_val;
