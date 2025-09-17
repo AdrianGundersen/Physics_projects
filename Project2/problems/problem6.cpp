eb67a4287a5d7dc93c35594424f11dcbfddd8fed
victorbj <victorbj@uio.no>
2025-09-17 13:46:04 +0200

    removed double main function

--- Project2/problems/problem6.cpp
+++ Project2/problems/problem6.cpp
@@ -10,26 +10,6 @@
 #include <iostream>
 
 int main(){
-    const int N = 11;
-    const double eps = 1e-8;
-    const int maxiter = 100000000;
-    bool converged = false;
-    int iterations = 0;
-
-    arma::mat R(N, N, arma::fill::eye);
-    arma::mat A = create_tridiagonal(N, -1.0, 2.0);
-    arma::vec eigenvalues;
-    jacobi_eigensolver(A, eps, eigenvalues, R, maxiter, iterations, converged);
-
-    for (int i = 0; i < 3 && i < eigenvalues.n_elem; ++i){
-        std::cout << "eigenvalue:\n" << eigenvalues(i)
-                  << "\n\neigenvector:\n" << R.col(i) << "\n";
-    }
-    return 0;
-}
-
-
-int main(){
     int N = 11;
     double a = 2.0;
     double d = -1.0;
