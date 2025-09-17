444193003c6df49254e8c0a5998e89c5e867d0fb
victorbj <victorbj@uio.no>
2025-09-17 13:47:15 +0200

    Finalized the changes to folder structure

--- Project2/README.md
+++ Project2/README.md
@@ -26,11 +26,14 @@ Build all problems
 ```bash
 make
 ```
-```bash
 Or build a specific problem
+```bash
 make problem2      # -> build/problem2.exe
 make problem3      # -> build/problem3.exe
 make problem4      # -> build/problem4.exe
+make problem5      # -> build/problem5.exe
+make problem6      # -> build/problem6.exe
+
 ```
 
 Clean objects and executables:
@@ -43,6 +46,17 @@ From Project root:
 ./build/problem2.exe
 ./build/problem3.exe
 ./build/problem4.exe
+./build/problem5.exe
+./build/problem6.exe
+```
+
+Or build, then run:
+```bash
+make run2
+make run3
+make run4
+make run5
+make run6
 ```
 
 ### Plot
---
+++ Project2/tridiag.cpp
@@ -0,0 +1,45 @@
+#include "tridiag.hpp"
+#include <assert.h>
+
+arma::Mat<double> create_tridiagonal(int N, double a, double d){
+    arma::Mat<double> A(N, N, arma::fill::zeros);
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (i == j){
+                A(i, j) = d;
+            }
+            else if (i-j == 1){
+                A(i, j) = a;
+            }
+            else if (j-i == 1){
+                A(i, j) = a;
+            }
+        }
+    }
+    return A;
+}
+
+
+double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){
+    assert(A.is_square());
+    assert(A.n_rows > 1);
+
+    int N = A.n_rows;
+    k = 0;
+    l = 1;
+
+    double max_val = std::abs(A(k,l)); // initial max value
+
+    for (int j = 1; j < N; j++){ // columns first for cache efficiency
+        for (int i = 0; i < j; i++){  // i < j ensures upper triangle
+            double abs_val = std::abs(A(i, j));
+            if (abs_val > max_val){
+                max_val = abs_val;
+                k = i;
+                l = j;
+            }
+        }
+    }
+    return max_val;
+}
+
