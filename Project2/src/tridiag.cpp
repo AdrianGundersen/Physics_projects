974245fcd1540cba20765b47b4cada9352082a7d
victorbj <victorbj@uio.no>
2025-09-21 21:27:56 +0200

    Added some comments

--- Project2/src/tridiag.cpp
+++ Project2/src/tridiag.cpp
@@ -6,13 +6,13 @@ arma::Mat<double> create_tridiagonal(int N, double a, double d){
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
             if (i == j){
-                A(i, j) = d;
+                A(i, j) = d; // main diagonal
             }
             else if (i-j == 1){
-                A(i, j) = a;
+                A(i, j) = a; // lower diagonal
             }
             else if (j-i == 1){
-                A(i, j) = a;
+                A(i, j) = a; // upper diagonal
             }
         }
     }
