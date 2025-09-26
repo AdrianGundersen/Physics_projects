002c667307a9c0e97c181dd928c2d633d9e9388f
victorbj <victorbj@uio.no>
2025-09-26 15:41:52 +0200

    made a friend class to keep private data

--- Project3/include/Particle.hpp
+++ Project3/include/Particle.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include <armadillo>
 
+class PenningTrap; 
+
 class Particle {
-public:
+    friend class PenningTrap; // PenningTrap can access private
+
+private:
     double charge;
     double mass;
     arma::vec position;  
     arma::vec velocity;  
 
+    public:
     Particle(double charge, double mass, 
              const arma::vec& position, 
              const arma::vec& velocity); // constructor
