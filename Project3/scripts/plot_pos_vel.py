added correct units

Project3/scripts/plot_pos_vel.py
@@ -15,6 +15,7 @@ plt.rcParams.update({
     'legend.fontsize': 10,
     'figure.dpi': 300,
 })
+
 os.makedirs("data/plot", exist_ok=True)
 data1 = np.loadtxt("data/pos_vel_1.txt")
 data2 = np.loadtxt("data/pos_vel_2.txt")
@@ -33,9 +34,23 @@ def plot_component(t1, arr1, t2, arr2, comp_name, xlabel, ylabel, filename):
     plt.savefig(filename)
     plt.close()
 
-plot_component(t1, x1, t2, x2, "x(t)", "time (s)", "x-position", "data/plot/x_position_vs_time.pdf")
-plot_component(t1, y1, t2, y2, "y(t)", "time (s)", "y-position", "data/plot/y_position_vs_time.pdf")
-plot_component(t1, z1, t2, z2, "z(t)", "time (s)", "z-position", "data/plot/z_position_vs_time.pdf")
 
-plot_component(x1, vx1, x2, vx2, "v_x(x)", "x-position", "x-velocity", "data/plot/v_x_vs_x.pdf")
-plot_component(z1, vz1, z2, vz2, "v_z(z)", "z-position", "z-velocity", "data/plot/v_z_vs_z.pdf")
+plot_component(t1, x1, t2, x2, "$x(t)$",
+    r"$\text{time}~(\mu\text{s})$", r"$x\text{-position}~(\mu\text{m})$",
+    "data/plot/x_position_vs_time.pdf")
+
+plot_component(t1, y1, t2, y2, "$y(t)$",
+    r"$\text{time}~(\mu\text{s})$", r"$y\text{-position}~(\mu\text{m})$",
+    "data/plot/y_position_vs_time.pdf")
+
+plot_component(t1, z1, t2, z2, "$z(t)$",
+    r"$\text{time}~(\mu\text{s})$", r"$z\text{-position}~(\mu\text{m})$",
+    "data/plot/z_position_vs_time.pdf")
+
+plot_component(x1, vx1, x2, vx2, "$v_x(x)$",
+    r"$x\text{-position}~(\mu\text{m})$", r"$v_x~(\mu\text{m}/\mu\text{s})$",
+    "data/plot/v_x_vs_x.pdf")
+
+plot_component(z1, vz1, z2, vz2, "$v_z(z)$",
+    r"$z\text{-position}~(\mu\text{m})$", r"$v_z~(\mu\text{m}/\mu\text{s})$",
+    "data/plot/v_z_vs_z.pdf")