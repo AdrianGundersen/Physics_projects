2ca7b448b36221fab30edee9315fadd2eb91d3fa
victorbj <victorbj@uio.no>
2025-10-26 17:45:33 +0100

    write json from observables

--- Project4/include/ising/io/json_util.hpp
+++ Project4/include/ising/io/json_util.hpp
@@ -3,9 +3,12 @@
 #pragma once
 #include "ising/model.hpp"
 #include "ising/lattice.hpp"
-#include "nlohmann/json.hpp"
+#include "ising/observables.hpp"
+
 #include <string>
+#include <nlohmann/json.hpp>
 #include <cstdint>
+
 namespace ising::io {
     // usage: model_from_json(root.at("model"), model);
     inline void model_from_json(const nlohmann::json& jm, ising::Model& m) {
@@ -16,5 +19,12 @@ namespace ising::io {
         const int L = jl.at("L").get<int>();
         ising::Lattice lat(L);
         return lat;
+    }
+
+    inline void observables_to_json(nlohmann::json& j, const ising::Observables& obs) {
+        j["E"] = obs.E;
+        j["E2"] = obs.E2;
+        j["M"] = obs.M;
+        j["M2"] = obs.M2;
     }
 }
