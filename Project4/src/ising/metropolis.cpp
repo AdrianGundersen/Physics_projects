00beab804c6c915788cf7dd1d5d32b9790adf1ce
victorbj <victorbj@uio.no>
2025-10-29 18:23:19 +0100

    started metropolis

---
+++ Project4/src/ising/metropolis.cpp
@@ -0,0 +1,44 @@
+// src/ising/metropolis.cpp
+/*
+*/
+
+#include "ising/metropolis.hpp"
+#include "ising/lattice.hpp"
+#include "ising/model.hpp"
+#include "ising/observables.hpp"
+#include <random>
+
+namespace ising{
+    void Metropolis(Model& model, Lattice& lattice, int n_steps, double T, int seed) {
+        std::mt19937 generator(seed);
+        const int L = lattice.size();
+        const int N = lattice.num_spins();
+        std::uniform_int_distribution<int> dist_pos(0, L-1); // selects random spin
+        // later use std::uniform_int_distribution<int>(0, N-1); // to select from all spins
+        std::uniform_real_distribution<double> dist_r(0.0, 1.0); // acceptance prob
+
+
+        const double beta = 1.0 / T;
+        const double J = model.J;
+        
+        int i, j;
+        int right, left, up, down;
+
+        for (int step = 0; step < n_steps; ++step) {
+            i = dist_pos(generator);
+            j = dist_pos(generator);
+
+            const int s_old = lattice(i, j);
+
+            right = (i + 1) % L;
+            left  = (i - 1 + L) % L;
+            down  = (j + 1) % L;
+            up    = (j - 1 + L) % L;
+            
+
+
+
+        }
+
+    }
+}
