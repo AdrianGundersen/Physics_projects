1038ed38b630933722c1db9959e65db9f9ff8000
casperaj <casperaj@uio.no>
2025-11-04 23:21:07 +0100

    laget en løkke som kljører over ulike verdier for T

--- Project4/apps/Ln.cpp
+++ Project4/apps/Ln.cpp
@@ -20,12 +20,13 @@ using namespace ising;
 // like numpy linspace
 static std::vector<double> linspace(double start, double end,  int n){
     std::vector<double> v;
-    v.reserve(n);
+    v.reserve(std::max(1,n));
     if (n <= 1) {v.push_back(start); return v;}
     double step = (end - start) / (n - 1);
     for (int i = 0; i < n; ++i){
         v.push_back(start + i * step);
     }
+    return v;
 }
 
 int main(int argc, char** argv) { // argc and argv to get JSON file path (argc is number of arguments, argv is array of arguments)
@@ -47,57 +48,70 @@ int main(int argc, char** argv) { // argc and argv to get JSON file path (argc i
     ising::Model model;
     ising::io::model_from_json(j.at("model"), model); // populate model
 
-    ising::Lattice initial_lat = ising::io::lattice_from_json(j.at("lattice"));
-    ising::Result result = ising::mcmc_run(initial_lat, model, j);
-
-    // writing setup
-    nlohmann::json write_json = j.at("write_to_file");
-
-    // parameters for default filename
-    int L = initial_lat.size();
-    double T = j.at("simulation").at("temperature").get<double>();
-    std::string spin_config = j.at("model").at("spin_config").get<std::string>();
-
-    std::string default_file_name = "data/outputs/L" + std::to_string(L)
-                                    + "_T=" + std::to_string(T)
-                                    + "_spin=" + spin_config + ".txt";
-    std::string output_file_name;
-    if (write_json.value("output_filename", "default") == "default") {
-        output_file_name = default_file_name;
+    ising::simParams params;
+    ising::io::simparams_from_json(j.at("simulation"), j.at("lattice"), params);
+    const bool use_Trange = params.use_Trange;
+    
+    std::vector<double> T_values;
+    if (use_Trange){
+        T_values = linspace(params.Tmin, params.Tmax, params.Tsteps);
     } else {
-        output_file_name = write_json.at("output_filename").get<std::string>();
-    }
+        ising::Lattice initial_lat = ising::io::lattice_from_json(j.at("lattice"));
+        double T = params.temperature;
+        ising::Result result = ising::mcmc_run(initial_lat, model, j);
 
-    ising::io::write_results_to_file(write_json, result, output_file_name);
-
-    std::vector<double> eps, eps2 , mabs, mabs2;
-
-
-    const auto& walkers = result.all_walkers;
-    for (const auto& w : walkers){
-        const int n = std::min<int>(w.eps_samples.size(), w.mabs_samples.size());
-        for (int i = 0; i < n; ++i) {
-            eps.push_back(w.eps_samples[i]);
-            mabs.push_back(w.mabs_samples[i]);
-            eps2.push_back(w.eps_samples[i] * w.eps_samples[i]);
-            mabs2.push_back(w.mabs_samples[i] * w.mabs_samples[i]);
+        // writing setup
+        nlohmann::json write_json = j.at("write_to_file");
+    
+    
+        // parameters for default filename
+        //double T = j.at("simulation").at("temperature").get<double>();
+        std::string spin_config = j.at("model").at("spin_config").get<std::string>();
+        int L = initial_lat.size();
+    
+        std::string default_file_name = "data/outputs/L" + std::to_string(L)
+                                        + "_T=" + std::to_string(T)
+                                        + "_spin=" + spin_config + ".txt";
+        std::string output_file_name;
+        if (write_json.value("output_filename", "default") == "default") {
+            output_file_name = default_file_name;
+        } else {
+            output_file_name = write_json.at("output_filename").get<std::string>();
         }
+        ising::io::write_results_to_file(write_json, result, output_file_name);
     }
-    double avg_eps = ising::average(eps);
-    double avg_mabs = ising::average(mabs);
-    double avg_eps2 = ising::average(eps2);
-    double avg_mabs2 = ising::average(mabs2);
-    double heat_cap = ising::heat_capacity(initial_lat, avg_eps2, avg_eps, T);
-    double susc = ising::susceptibility(initial_lat, avg_mabs2, avg_mabs, T);
 
-    std::cout << "avgerage absolute magnetization per spin <|m|>: " << avg_mabs << "\n";
-    std::cout << "average energy per spin <ε>: " << avg_eps << "\n";
-    std::cout << "average energy squared per spin <ε²>: " << avg_eps2 << "\n";
-    std::cout << "average magnetization squared per spin <m²>: " << avg_mabs2 << "\n";
-    std::cout << "heat capacity: " << heat_cap << "\n";
-    std::cout << "susceptibility: " << susc << "\n";
+    for (double T : T_values){
+        json jT = j;
+        jT["simulation"]["temperature"] = T;
+        ising::Lattice initial_lat = ising::io::lattice_from_json(jT.at("lattice"));
+        ising::Result result = ising::mcmc_run(initial_lat, model, jT);
+        std::vector<double> eps, eps2 , mabs, mabs2;
 
-    //sdt::cout << "heat capasity: " << result.heat_cap << "\n";
-    //sdt::cout << "susceptibility: " << result.susc << "\n";
+        const auto& walkers = result.all_walkers;
+        for (const auto& w : walkers){
+            const int n = std::min<int>(w.eps_samples.size(), w.mabs_samples.size());
+            for (int i = 0; i < n; ++i) {
+                eps.push_back(w.eps_samples[i]);
+                mabs.push_back(w.mabs_samples[i]);
+                eps2.push_back(w.eps_samples[i] * w.eps_samples[i]);
+                mabs2.push_back(w.mabs_samples[i] * w.mabs_samples[i]);
+            }
+        }
+        double avg_eps = ising::average(eps);
+        double avg_mabs = ising::average(mabs);
+        double avg_eps2 = ising::average(eps2);
+        double avg_mabs2 = ising::average(mabs2);
+
+        double heat_cap = ising::heat_capacity(initial_lat, avg_eps2, avg_eps, T);
+        double susc = ising::susceptibility(initial_lat, avg_mabs2, avg_mabs, T);
+
+        std::cout << "avgerage absolute magnetization per spin <|m|>: " << avg_mabs << "\n";
+        std::cout << "average energy per spin <ε>: " << avg_eps << "\n";
+        std::cout << "average energy squared per spin <ε²>: " << avg_eps2 << "\n";
+        std::cout << "average magnetization squared per spin <m²>: " << avg_mabs2 << "\n";
+        std::cout << "heat capacity: " << heat_cap << "\n";
+        std::cout << "susceptibility: " << susc << "\n";
+    }
     return 0;
 }
