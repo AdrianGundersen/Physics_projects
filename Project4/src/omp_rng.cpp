a6f5c8dca3a2ba720f06fa38e5b636b7edf0b3fd
victorbj <victorbj@uio.no>
2025-11-09 17:38:47 +0100

    changed seed drawing

--- Project4/src/omp_rng.cpp
+++ Project4/src/omp_rng.cpp
@@ -12,13 +12,15 @@ namespace omp_rng
 
   // Initialize the omp_rng_container according to the number of threads
   std::vector<uint32_t> initialize_omp_rng_container(uint32_t base_seed, int walkers) {
-    std::seed_seq seq{base_seed, base_seed*2}; // need a range of numbers
-    std::vector<std::uint32_t> seeds(walkers);
-
-    seq.generate(seeds.begin(), seeds.end());
-    
+    std::vector<std::uint32_t> seeds;
+    seeds.reserve(walkers);
+    for (int i = 0; i < walkers; i++) {
+        std::seed_seq ss{base_seed, static_cast<std::uint32_t>(i)}; // unique seed for each walker
+        std::uint32_t s = 0; // temp variable
+        ss.generate(&s, &s + 1);   // generate a new seed
+        seeds.push_back(s);
+    }
     return seeds;
-  }
 
     
 
