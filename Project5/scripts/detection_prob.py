641b70682f5b45dd3f4d9401131eb47c3e3805b9
casperaj <casperaj@uio.no>
2025-11-27 15:51:19 +0100

    edited detection plot to have correct stepsize

--- Project5/scripts/detection_prob.py
+++ Project5/scripts/detection_prob.py
@@ -3,6 +3,20 @@ import matplotlib.pyplot as plt
 import os
 from io_python import read_prob_file
 
+# plotting style
+plt.rcParams.update({
+    'font.family': 'serif',
+    'font.size': 12,
+    'figure.figsize': (6, 4),
+    'axes.titlesize': 14,
+    'axes.labelsize': 20,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'lines.linewidth': 2.0,
+    'legend.fontsize': 14,
+    'figure.dpi': 300,
+})
+
 output_dir = "output/figures"
 os.makedirs(output_dir, exist_ok=True)
 
@@ -17,25 +31,25 @@ def plot_screen_distribution(prob_fields, dt, T, x_screen=0.8, L=1.0, slits="sin
     t_index = int(T / dt)
     field = prob_fields[t_index]      
     M = field.shape[0]
-    dx = L / M
+    dx = L / (M - 1)
 
     # index to find x_screen
-    j_screen = int(x_screen / dx)
+    j_screen = int(round(x_screen / dx))
 
     # probability along the screen
     line_prob = field[j_screen, :]     # |psi(x_screen, y_i, T)|^2
 
     # p(y | x_screen, T)
-    p_y_given_x = line_prob / np.sum(line_prob)
+    p_y_given_x = line_prob / (np.sum(line_prob))
+    print("Sum p(y|x) =", np.sum(p_y_given_x))
 
-    y = (np.arange(M) + 0.5) * dx
+    y = (np.arange(M)) * dx
 
 
     plt.figure()
     plt.plot(y, p_y_given_x)
     plt.xlabel(r"$y$")
     plt.ylabel(r"$p(y \mid x=0.8;\, t=0.002)$")
-    plt.title("Deteksjonssannsynlighet langs skjermen ved $x=0.8$")
     plt.tight_layout()
     out_path = os.path.join(output_dir, fileneme)
     plt.savefig(out_path)
@@ -44,10 +58,10 @@ def plot_screen_distribution(prob_fields, dt, T, x_screen=0.8, L=1.0, slits="sin
 
 
 if __name__ == "__main__":
-    filename = "output/wavefunction_p8.txt"  # juster sti
+    filename = "output/wavefunction_p8_200.txt"  # juster sti
     prob_fields = read_prob_file(filename)
 
-    slits = "double"
+    slits = "200grid"
 
     dt = 2.5e-5
     T = 0.002
