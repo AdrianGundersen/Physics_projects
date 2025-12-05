e0cd670424d610b6015602835f829faec1db4281
casperaj <casperaj@uio.no>
2025-12-05 20:29:43 +0100

    more comments

--- Project5/include/io.hpp
+++ Project5/include/io.hpp
@@ -1,25 +1,19 @@
 //include/io.hpp
-
+/*
+1. params_from_json read simulation parameters from a JSON object and populate the corresponding structures.
+2. Function to write probability density to a file. To use uncomment in solver.cpp
+3. Function to write wavefunction to a file. 
+*/
 #pragma once
 #include <string>
 #include "constants.hpp"
 #include <nlohmann/json.hpp>
 
 namespace ds {
-   /*
-   Function to read simulation parameters from a JSON object and populate the corresponding structures.
-   Same structure as from ising model project.
-   */
+   
     void params_from_json(const nlohmann::json& j, ds::simParams& params, ds::Grid& grid, ds::SolverParams& solver_params, ds::PotentialParams& potential_params, ds::OutputParams& output_params);
-    /*
-    Function to write probability density to a file.
-    The function takes the filename, probability density vector, timestep, and precision as inputs.
-    To use uncomment the call for the function in solver.cpp
-    */
+    
     void write_prob_to_file(const std::string& filename, const ds::rvec& prob_density, Index timestep, Index precision = 6);
-    /*
-    Same as above but for wavefunction includeing real and complex numbers.
-    This is used in solver.cpp and the probabilities as calculated in python.
-    */
+    
     void write_wavefunction_to_file(const std::string& filename, const ds::cvec& wavefunction, Index timestep, Index precision = 6);
 }
--- Project5/include/potential.hpp
+++ Project5/include/potential.hpp
@@ -1,4 +1,11 @@
 // include/potential.hpp
+
+/*
+Functuin to initialize the potential based on the provided parameters.
+Takes a Potential object to fill, the grid information, and potential parameters.
+The potensial creates a box, a crosssection berrier with slits, or a harmonic oscillator potential.
+*/
+
 #pragma once
 #include "constants.hpp"
 #include <vector>
@@ -9,11 +16,6 @@ namespace ds {
         ds::rvec values;
     };
 
-    /*
-    Functuin to initialize the potential based on the provided parameters.
-    Takes a Potential object to fill, the grid information, and potential parameters.
-    The potensial creates a box, a crosssection berrier with slits, or a harmonic oscillator potential.
-    */
     void initialize_potential(ds::Potential& V, const ds::Grid& grid, 
                             const ds::PotentialParams& potential_params);
 } // namespace ds
