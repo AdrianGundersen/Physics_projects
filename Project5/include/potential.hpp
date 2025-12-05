91891ec684f2cb9ce35d9e57543d4ab3d3cee1c2
casperaj <casperaj@uio.no>
2025-12-05 17:13:22 +0100

    comments

--- Project5/include/potential.hpp
+++ Project5/include/potential.hpp
@@ -9,6 +9,11 @@ namespace ds {
         ds::rvec values;
     };
 
+    /*
+    Functuin to initialize the potential based on the provided parameters.
+    Takes a Potential object to fill, the grid information, and potential parameters.
+    The potensial creates a box, a crosssection berrier with slits, or a harmonic oscillator potential.
+    */
     void initialize_potential(ds::Potential& V, const ds::Grid& grid, 
                             const ds::PotentialParams& potential_params);
 } // namespace ds
